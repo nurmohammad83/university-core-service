374f827a88c4e1e4f370481255dce2eb8c4abbe5
Nur Mohammad <mohammadnur5480@gmail.com>
2023-08-29 08:23:40 +0600

    Added controler

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
-import catchAsync from '../../../shared/catchAsync';
-import { SemesterRegistrationService } from './semesterRegistration.service';
-import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { semesterRegistrationFilterableFields } from './semesterRegistration.constans';
+import { SemesterRegistrationService } from './semesterRegistration.service';
 
 const insertIntoDb = catchAsync(async (req: Request, res: Response) => {
   const result = await SemesterRegistrationService.insertIntoDb(req.body);
@@ -13,5 +16,19 @@ const insertIntoDb = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-
-export const SemesterRegistrationController = { insertIntoDb };
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, semesterRegistrationFilterableFields);
+  const options = pick(req.query, paginationFields);
+  const result = await SemesterRegistrationService.getAllFromDB(
+    filters,
+    options
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'SemesterRegistrations fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+export const SemesterRegistrationController = { insertIntoDb, getAllFromDB };
