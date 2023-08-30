c409b0a6278cd1da9e7a4d70428382f8e4e30996
Nur Mohammad <mohammadnur5480@gmail.com>
2023-08-30 06:44:36 +0600

    offered course validation added

--- src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
@@ -1,9 +1,19 @@
 import { OfferedCourseSection } from '@prisma/client';
 import prisma from '../../../shared/prisma';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
 
-const insertIntoDb = async (
-  data: OfferedCourseSection
-): Promise<OfferedCourseSection> => {
+const insertIntoDb = async (data: any): Promise<OfferedCourseSection> => {
+  const isExist = await prisma.offeredCourse.findFirst({
+    where: {
+      id: data.offeredCourseId,
+    },
+  });
+
+  data.semesterRegistrationId = isExist?.semesterRegistrationId;
+  if (!isExist) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Data dose not exist');
+  }
   const result = await prisma.offeredCourseSection.create({ data });
   return result;
 };
